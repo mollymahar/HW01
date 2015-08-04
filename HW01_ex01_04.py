c7ce42404179b550cf8034d3cc84ab5ba622fe99
mollymahar <mollymahar@gmail.com>
2015-08-04 12:34:23 -0700

    Molly testing the commit/merge

--- HW01_ex01_04.py
+++ HW01_ex01_04.py
@@ -1,6 +1,6 @@
 # HW01_ex01_04
 # Start the Python interpreter and use it as a calculator. 
-# Python’s syntax for math operations is almost the same as 
+# Python's syntax for math operations is almost the same as 
 # standard mathematical notation. For example, the symbols 
 # +, - and / denote addition, subtraction and division, as you would expect. 
 # The symbol for multiplication is *.
@@ -9,3 +9,5 @@
 # average time per mile? What is your average speed in miles per hour? 
 # (Hint: there are 1.61 kilometers in a mile).
 # Average Speed in MPH:
+
+print((10/1.61)/(43.5/60))
